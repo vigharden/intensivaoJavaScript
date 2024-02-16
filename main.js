18113a6c38166e045433603a95c24a8db2d652eb
vigharden <108622978+vigharden@users.noreply.github.com>
2024-02-16 15:17:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,92 @@
+//--------------------- 1
+// alert ("Hello world")
+
+//--------------------- 2
+
+// let firstNumber = 3
+// let secondNumber = 2
+
+// result = (firstNumber) + (secondNumber)
+
+// console.log (result)
+
+// alert (result)
+
+//--------------------- 3
+
+// let randomNumber = 8
+
+// console.log(randomNumber)
+
+// if(typeof randomNumber === 'number') {
+//     console.log("É um número")
+// } else {
+//     console.log("Não é um número")
+// }
+
+// -------------------- 4
+
+// let isItAString = 3
+
+// if(typeof isItAString === 'string') {
+//     console.log("É uma string")
+// } else {
+//     console.log("Não é uma string")
+// }
+
+//--------------------- 5
+
+// let isItABoolean = true
+
+// if(typeof isItABoolean === 'boolean') {
+//     console.log("É um booleano")
+// } else {
+//     console.log("Não é um booleano")
+// }
+
+//-------------------- 6
+
+// let sub1 = 6
+// let sub2 = 4
+
+// console.log("A subtração dos números e: " + (sub1 - sub2))
+
+//-------------------- 7
+
+// let mult1 = 4
+// let mult2 = 2
+
+// console.log("A multiplicação desses números e: " + (mult1 * mult2))
+
+//-------------------- 8
+
+// let div1 = 4
+// let div2 = 2
+
+// console.log("A divisão dos números e :" + (div1 / div2))
+
+//------------------- 9 
+
+// let firstNumber = 8
+
+// let restDiv = firstNumber % 2
+
+// if(restDiv == 0) {
+//     console.log("É um número par")
+// } else {
+//     console.log("Não é um número par")
+// }
+
+//-------------------- 10
+
+let firstNumber = 10
+
+let restDiv = firstNumber % 2
+
+alert(restDiv)
+
+if(restDiv != 0) {
+    console.log("É um número ímpar")
+} else {
+    console.log("Não é um número ímpar")
+}
